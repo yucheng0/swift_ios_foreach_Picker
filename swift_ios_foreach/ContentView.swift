7637986f83bf561f61531e86afdfd302df7aee85
yuchengtseng <yucheng0939@gmail.com>
2020-12-11 10:44:25 +0800

    Initial Commit

---
+++ swift_ios_foreach/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  swift_ios_foreach
+//
+//  Created by yuchengtseng on 2020/12/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
